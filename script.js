5e1a9782c6695a3c99d6b8b6c1831c25365aef4a
Casper Wortmann <casper@MacBook-Pro-van-Casper-2.local>
2023-03-15 13:39:41 +0100

    docs.

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ console.log("Vid6");
 // let modelsUrl = "./models"
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/docs'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/docs'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/docs'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/docs')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/japan_quotes/docs'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/japan_quotes/docs'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/japan_quotes/docs'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/japan_quotes/docs')
   // faceapi.nets.tinyFaceDetector.loadFromUri(modelsUrl + 'tiny_face_detector_model-weights_manifest.json'),
   // faceapi.nets.faceLandmark68Net.loadFromUri(modelsUrl + 'face_landmark_68_model-weights_manifest.json'),
   // faceapi.nets.faceRecognitionNet.loadFromUri(modelsUrl + 'face_recognition_model-weights_manifest.json'),
